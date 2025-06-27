79c2dcdb3e5b73e543a3f0151d4e7d26dbf0815a
jcub-gold <jacobgoldbergredsox@gamil.com>
2025-06-27 11:52:16 -0700

    initial packaging of record3d data

---
+++ src/utils/data_utils.py
@@ -0,0 +1,110 @@
+import os
+import open3d as o3d
+import numpy as np
+import json
+import cv2
+from scipy.spatial.transform import Rotation
+import OpenEXR
+import Imath
+
+"""
+    Function: get_number
+    --------------------
+    word: string containing a number
+    
+    Returns the integer value of the number found in the string.
+"""
+def get_number(word):
+    numbers = ""
+    for char in word:
+        if char.isnumeric():
+            numbers += char
+    
+    return int(numbers)
+
+"""
+    Function: read_exr_depth
+    ------------------------
+    exr_path: path to the .exr file containing depth data
+
+    Reads the depth data from an OpenEXR file and returns it as a numpy array.
+"""
+def read_exr_depth(exr_path):
+    exr_file = OpenEXR.InputFile(exr_path)
+    dw = exr_file.header()['dataWindow']
+    width = dw.max.x - dw.min.x + 1
+    height = dw.max.y - dw.min.y + 1
+    pt = Imath.PixelType(Imath.PixelType.HALF)
+    depth_str = exr_file.channel("R", pt)
+    depth = np.frombuffer(depth_str, dtype=np.float16).astype(np.float32)
+    depth = depth.reshape((height, width))
+    return depth
+
+"""
+    Function: prepare_record3d_data
+    -------------------------------
+    images_dir: path to object_{n}/images
+    depth_dir: path to input_depth
+    metadata_path: path to new_metadata.json
+
+    Sorts the frames based on their number, reads the metadata,
+    and prepares the data for each frame including the image, depth map, and extrinsics.
+"""
+def prepare_record3d_data(images_dir, depth_dir, metadata_path):
+
+    frames = [get_number(os.path.splitext(p)[0]) for p in os.listdir(images_dir)]
+    frames.sort()
+
+    with open(metadata_path, 'r') as file:
+        metadata_dict = json.load(file)
+
+    poses_data = np.array(metadata_dict["poses"])
+
+    W, H = metadata_dict["w"], metadata_dict["h"]
+    K = np.array(metadata_dict["K"]).reshape((3, 3)).T
+    focal_length = K[0, 0]
+
+    intrinsics = o3d.camera.PinholeCameraIntrinsic(
+        width=W, 
+        height=H,
+        fx=focal_length,
+        fy=focal_length, 
+        cx=W/2, 
+        cy=H/2 
+    )
+    
+    output = {
+        "h": H,
+        "w": W,
+        "intrinsics": intrinsics,
+        "frames": {}
+    }
+
+    for frame in frames:
+        img_file = os.path.join(images_dir, f"{frame}.png")
+
+        if not os.path.isfile(img_file):
+            img_file = os.path.join(images_dir, f"{frame}.jpg")
+
+        exr_file = os.path.join(depth_dir, f"{frame}.exr")
+
+        mask_img = cv2.imread(img_file, cv2.IMREAD_UNCHANGED)
+        mask_img = cv2.cvtColor(mask_img, cv2.COLOR_BGRA2RGBA)
+
+        depth_img = read_exr_depth(exr_file)
+        depth_img = cv2.resize(depth_img, dsize=(mask_img.shape[1], mask_img.shape[0]), interpolation=cv2.INTER_CUBIC)
+
+        extrinsics = np.eye(4)
+        rotation = Rotation.from_quat(poses_data[frame][:4]).as_matrix()
+        translation = poses_data[frame][4:]
+        extrinsics[:3, :3] = rotation
+        extrinsics[:3, 3] = translation
+
+        flip_mat = np.eye(4)
+        flip_mat[1, 1] = -1
+        flip_mat[2, 2] = -1
+        extrinsics = flip_mat @ np.linalg.inv(extrinsics)
+        
+        output["frames"][frame] = [mask_img, depth_img, extrinsics]
+
+    return output
---
+++ test/test_utils.py
@@ -0,0 +1,4 @@
+import src.utils.data_utils as data_utils
+
+if __name__ == "__main__":
+    print("Testing data_utils...")
